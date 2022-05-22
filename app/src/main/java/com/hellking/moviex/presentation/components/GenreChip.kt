e7b7e37b58925fb9a220ab78f83e24b8a5d230d8
Bhaskar <arbkm22@gmail.com>
2022-05-22 15:58:17 +0530

    Changed GenreChip Card radius  - made it more 'aesthetic'

--- app/src/main/java/com/hellking/moviex/presentation/components/GenreChip.kt
+++ app/src/main/java/com/hellking/moviex/presentation/components/GenreChip.kt
@@ -1,11 +1,9 @@
 package com.hellking.moviex.presentation.components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.hellking.moviex.domain.models.MovieDetailUser
-import com.hellking.moviex.presentation.ui.theme.Shapes
 
 @Composable
 fun GenreChip(
@@ -22,7 +19,7 @@ fun GenreChip(
     Surface(
         modifier = Modifier.padding(6.dp),
         elevation = 8.dp,
-        shape = RoundedCornerShape(8.dp),
+        shape = RoundedCornerShape(24.dp),
         color = Color.DarkGray
     ) {
         Row {
